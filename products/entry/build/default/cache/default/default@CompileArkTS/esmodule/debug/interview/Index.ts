7dc49ca439fdfcf0ccb5bae4c71dc073917be9aa
userName <userEmail>
2026-03-16 20:25:11 +0800

    第2次提交

--- products/entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/interview/Index.ts
+++ products/entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/interview/Index.ts
@@ -3,10 +3,10 @@ export { InterviewPage } from "@bundle:com.example.zhiheng/entry@interview/ets/p
 export function harInit(builderName: string): void {
     // 动态引入要跳转的页面
     switch (builderName) {
-        case BuilderNameConstants.HARA_A1:
+        case BuilderNameConstants.INTERVIEW_INTRODUCTION:
             import("@bundle:com.example.zhiheng/entry@interview/ets/pages/introduction");
             break;
-        case BuilderNameConstants.HARA_A2:
+        case BuilderNameConstants.INTERVIEW_DIALOG:
             import("@bundle:com.example.zhiheng/entry@interview/ets/pages/dialog");
             break;
         default:
